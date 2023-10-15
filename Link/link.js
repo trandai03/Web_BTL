848e6e53035e70104e17401a030b83543da69a61
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2023-10-15 16:24:40 +0700

    Sửa js trang link
    Sửa js trang link

--- Link/link.js
+++ Link/link.js
@@ -1,39 +1,95 @@
 
-        let reactions = {
-            like: 0,
-            love: 0,
-            haha: 0
-        };
+const reactButton = document.querySelector('.react-button');
+const reactClick = document.querySelector('.react-click');
+const reactButtons = document.querySelectorAll('.button-react');
+const likeCountElement = document.querySelector('.like-number');
+const tymCountElement = document.querySelector('.tym-number');
+const clapCountElement = document.querySelector('.clapping-number');
+const starCountElement = document.querySelector('.star-number');
+const totalCountElement = document.querySelector('.total');
 
-        function reactToPost(reaction) {
-            if (reaction in reactions) {
-                reactions[reaction]++;
-                updateReactionCount(reaction);
-                updateTotalReactions();
+var likeCount = parseInt(likeCountElement.textContent); 
+var tymCount = parseInt(tymCountElement.textContent); 
+var clapCount = parseInt(clapCountElement.textContent);
+var starCount = parseInt(starCountElement.textContent);
+var total = parseInt(totalCountElement.textContent);
+
+
+var reactClickVisible = false;
+
+
+reactButton.addEventListener('click', function() {
+    if (!reactClickVisible) {
+
+        reactClick.style.display = 'flex';
+        reactClickVisible = true;
+    } else {
+  
+        reactClick.style.display = 'none';
+        reactClickVisible = false;
+    }
+});
+
+const reactionStates = {
+    btnReactionLike: false, 
+    btnReactionLove: false, 
+    btnReactionStar: false,
+    btnReactionClap: false,
+};
+
+var previousReactionButton = null;
+
+
+reactButtons.forEach((button) => {
+    button.addEventListener('click', function() {
+        const reactionName = this.getAttribute('name');
+
+        if (previousReactionButton === button) {
+         
+            if (reactionName === 'btnReactionLike') {
+                likeCount--;
+            } else if (reactionName === 'btnReactionLove') {
+                tymCount--;
+            }else if (reactionName === 'btnReactionStar') {
+                starCount--;
+            }else if (reactionName === 'btnReactionClap') {
+                clapCount--;
             }
-        }
-        function updateReactionCount(reaction) {
-            const reactionCountElement = document.querySelector(`.${reaction}-count`);
-            reactionCountElement.textContent = reactions[reaction];
-        }
-        function updateTotalReactions() {
-            const totalElement = document.querySelector('.reaction-total');
-            const total = reactions.like + reactions.love + reactions.haha;
-            totalElement.textContent = `Da Nhan: ${total}`;
-        }
-// Initialize variables outside the function
-let isLiked = false;
-let likeCount = 0;
+            
+            previousReactionButton = null; 
+        } else {
+            
+            if (previousReactionButton) {
+                const previousReactionName = previousReactionButton.getAttribute('name');
+                if (previousReactionName === 'btnReactionLike') {
+                    likeCount--;
+                } else if (previousReactionName === 'btnReactionLove') {
+                    tymCount--;
+                }else if (previousReactionName === 'btnReactionStar') {
+                    starCount--;
+                }else if (previousReactionName === 'btnReactionClap') {
+                    clapCount--;
+                }
+            }
 
-function toggleLike(element) {
-  if (isLiked) {
-    likeCount--;
-  } else {
-    likeCount++;
-  }
+            if (reactionName === 'btnReactionLike') {
+                likeCount++;
+            } else if (reactionName === 'btnReactionLove') {
+                tymCount++;
+            } else if (reactionName === 'btnReactionStar') {
+                starCount++;
+            }else if (reactionName === 'btnReactionClap') {
+                clapCount++;
+            }
 
-  isLiked = !isLiked;
+            previousReactionButton = button; 
+        }
 
-  element.nextElementSibling.textContent = likeCount;
-  element.classList.toggle("active", isLiked);
-}
+        total = likeCount + tymCount + starCount + clapCount;
+        likeCountElement.textContent = likeCount;
+        tymCountElement.textContent = tymCount;
+        starCountElement.textContent = starCount;
+        clapCountElement.textContent = clapCount;
+        totalCountElement.textContent = total;
+    });
+});
